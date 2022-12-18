0b21b320726172fe80d024b80528de693af99c4a
Asfak Shahrier <59991886+asfak27@users.noreply.github.com>
2022-12-18 18:58:15 +0600

    Add files via upload

---
+++ mynewjs.js
@@ -0,0 +1,17 @@
+/*function sum(x,y)
+{
+    return x+y;
+}
+let x=sum(5,10);
+console.log("Hello World");
+console.log("Sum of two number = ",x);*/
+
+
+const prompt = require("prompt-sync")();
+
+var first=prompt("");
+
+var last=prompt("");
+
+console.log("Your full name is:",first,last);
+
